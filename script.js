e5ade2eea95d56aa0b5d37c18a996fe1cd562b47
Sakibul-05 <110473516+Sakibul-05@users.noreply.github.com>
2023-06-22 10:47:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let output = document.getElementById("output");
+function makePromise() {
+	return new Promise((reslove,reject)=>{
+		setTimeout(()=>{
+			reslove("Hello, world");
+		},1000);
+	})
+}
+makePromise().then((data)=>{
+	output.innerText = data;
+})
